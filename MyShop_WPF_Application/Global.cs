6160da01c499bcb2c759f0416d2448c87730f374
ptminngoc <pngoc0105@gmail.com>
2023-04-28 02:36:39 +0700

    delete folder icon

--- MyShop_WPF_Application/Global.cs
+++ MyShop_WPF_Application/Global.cs
@@ -11,7 +11,7 @@ namespace MyShop_WPF_Application
     // this class contains everything value that needed throughout the entire app
     internal class Global
     {
-        public static string ConnectionString = "Server = DESKTOP-F3ASQE2;\r\nDatabase = MyShopDB;\r\nTrusted_Connection=yes;\r\nTrustServerCertificate=True";
+        public static string ConnectionString = "Server = .\\SQLExpress;\r\nDatabase = MyShopDB;\r\nTrusted_Connection=yes;\r\nTrustServerCertificate=True";
         public static int role = -1;
         public static Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
         public static SqlConnection? Connection;
